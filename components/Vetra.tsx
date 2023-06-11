b134508d5a7f386188ddde6a3da3e74b1053ac85
Linh14dev <Linh14dev@gmail.com>
2023-06-12 00:23:45 +0700

    Refactor Vetra Component

--- components/Vetra.tsx
+++ components/Vetra.tsx
@@ -1,18 +1,12 @@
-import {
-  appBehavior,
-  toggleNotificationLayout,
-} from '@/store/slices/app-behavior'
 import React, {ReactNode} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+
 type VetraProps = {
   children: ReactNode
 }
 
 const Vetra: React.FC<VetraProps> = ({children}: VetraProps) => {
-  const vetra = useSelector(
-    (state: appBehavior) => state.appBehavior.isVetraMove,
-  )
-  return <main className={`vetra ${vetra && 'move'}`}>{children}</main>
+
+  return <main className="vetra">{children}</main>
 }
 
 export default Vetra
