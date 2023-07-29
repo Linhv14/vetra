8c4829e55853598d1c8ea0835b0f9da90dc232c0
Linh14dev <Linh14dev@gmail.com>
2023-07-30 00:58:57 +0700

    Add Public key

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ type LayoutProps = {children: ReactNode}
 
 const SetupLayout: React.FC<LayoutProps> = ({children}: LayoutProps) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={"pk_test_c2FjcmVkLWtpdHRlbi0zMi5jbGVyay5hY2NvdW50cy5kZXYk"}>
       <html lang="en">
         <head>
           <link
